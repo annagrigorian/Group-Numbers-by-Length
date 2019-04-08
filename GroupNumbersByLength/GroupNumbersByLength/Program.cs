e1db564509f8cef76768cdd405922bfdda0fd68a
Anna Grigorian <anna.grigorian11@gmail.com>
2019-04-08 18:44:05 +0400

    Grouped by Length

---
+++ GroupNumbersByLength/GroupNumbersByLength/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupNumbersByLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 1000);
+
+            var groups = numbers.GroupBy(i => i.ToString().Length);
+
+            foreach (var item in groups)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var itemo in item)
+                {
+                    Console.Write(itemo + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
