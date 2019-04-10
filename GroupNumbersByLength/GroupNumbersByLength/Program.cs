d59f8c894be8d20dcb28ad6a0743159a96aa0524
Anna Grigorian <anna.grigorian11@gmail.com>
2019-04-10 17:58:12 +0400

    With Group Key

--- GroupNumbersByLength/GroupNumbersByLength/Program.cs
+++ GroupNumbersByLength/GroupNumbersByLength/Program.cs
@@ -10,19 +10,21 @@ namespace GroupNumbersByLength
     {
         static void Main(string[] args)
         {
-            var numbers = Enumerable.Range(0, 1000);
+            var numbers = Enumerable.Range(0, 100);
 
             var groups = numbers.GroupBy(i => i.ToString().Length);
 
             foreach (var item in groups)
             {
-                Console.WriteLine(item.Key);
+                Console.WriteLine($"Number of digits : {item.Key}");
+                Console.WriteLine("Group numbers : ");
                 foreach (var itemo in item)
                 {
                     Console.Write(itemo + " ");
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
     }
 }
